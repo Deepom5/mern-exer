58a11b902b93a04aa3b0a3821ea2708e7d10329b
Deepom5 <64247104+Deepom5@users.noreply.github.com>
2021-06-05 12:32:17 +0530

    Update navbar.component.js

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,35 +1,25 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
-export default class Navbar extends Component {
+import { Navbar, Nav } from 'react-bootstrap';
+export default class Navbarh extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">Syook</Link>
-        <button className="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-        <div className="collpase navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto">
-          <li className="navbar-item">
-          <Link to="/" className="nav-link">home</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/order" className="nav-link">Create Order</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/item" className="nav-link">Create Item</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/customer" className="nav-link">Create customer</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/vehicle" className="nav-link">Create vehicle</Link>
-          </li>
-        </ul>
-        </div>
-      </nav>
+      <Navbar bg="dark" variant="dark" expand="lg">
+  <Navbar.Brand href="/">Syook</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="ml-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/order">Create Order</Nav.Link>
+      <Nav.Link href="/item">Create Item</Nav.Link>
+      <Nav.Link href="/customer">Create customer</Nav.Link>
+      <Nav.Link href="/vehicle">Create vehicle</Nav.Link>
+    </Nav>
+  
+  </Navbar.Collapse>
+</Navbar>
+       
+       
     );
   }
 }
